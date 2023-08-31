21bda34c1683ad17618f57cbaf6781de4f2591d8
Vnu619 <34149646+Vnu619@users.noreply.github.com>
2023-08-31 23:23:07 +0800

    navigation files added
    navigation params and pathplanner

--- smorphi_controller/scripts/smorphi_controller.py
+++ smorphi_controller/scripts/smorphi_controller.py
@@ -15,12 +15,13 @@ import serial
 
 class WASPMini(object):
 	def __init__(self):
+		self.imu_flag = 0
 		self.wheel_odom_pub = rospy.Publisher('wheel_odom', Odometry, queue_size=50)
 		rospy.Subscriber('/cmd_vel', Twist, self.writespeed)
 		rospy.Subscriber('/imu_data', Imu, self.imucallback)
 		rospy.Subscriber("/shape_need", Int32, self.shpneed)
 		self.shape_pub = rospy.Publisher("current_shape", Int32, queue_size=50)
-		self.ser = serial.Serial('/dev/ttyUSB2',115200, timeout=0.005)
+		self.ser = serial.Serial('/dev/smorphi_mb',115200, timeout=0.005)
 
 		self.x = 0.0
 		self.y = 0.0
@@ -39,7 +40,6 @@ class WASPMini(object):
 		self.roll = 0
 		self.pitch = 0
 		self.prev_yaw = 0
-		self.imu_flag = 0
 		self.last_time = rospy.Time.now()
 
 
@@ -48,29 +48,26 @@ class WASPMini(object):
 			cm_Vx = "{:.2f}".format(float(msg.linear.x))
 			cm_Vy = "{:.2f}".format(float(msg.linear.y))
 			cm_Wz = "{:.2f}".format(float(msg.angular.z))
-			cm_shape = shape_need
+			cm_shape = self.shape_need
 			cmd_vel = str(cm_Vx)+","+str(cm_Vy)+","+str(cm_Wz)+","+str(cm_shape)+"\n"
 			#print (cmd_vel)
 			self.ser.write(cmd_vel.encode())
 
 	def shpneed(self, msg2):
-			global shape_need
-			shape_need = msg2.data
+			self.shape_need = msg2.data
 
 	def imucallback(self, msg):
 		self.imu_msg = msg
 		if self.imu_flag == 0:
 			(self.roll, self.pitch, self.prev_yaw) = tf.transformations.euler_from_quaternion([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
+			#print("imu_init=")
+			#print(self.prev_yaw)
 			self.imu_flag = 1
             
 	def encoder_read(self):
-		global vx
-		global vy
-		global w
-		global cur_shape
 		current_time = rospy.Time.now()
 		shape_liz = ["i","o","l"]
-		encoder_readings = str(ser.readline())
+		encoder_readings = str(self.ser.readline())
 
 		if len(encoder_readings)>5:
 			encoder_readings = encoder_readings[2:-5].split(",")
@@ -87,23 +84,26 @@ class WASPMini(object):
 				    encoder_values = [float(a) for a in encoder_values]
 				    self.vx = encoder_values[0]
 				    self.vy = encoder_values[1]
-				    self.w = encoder_values[2]
+				    #self.w = encoder_values[2]
 			else:
 				print("waiting")
 				
 			#print(vx,vy,w)
 		#b = "0"
 		(r, p, yaw) = tf.transformations.euler_from_quaternion([self.imu_msg.orientation.x, self.imu_msg.orientation.y, self.imu_msg.orientation.z, self.imu_msg.orientation.w])
+		
 		self.w = yaw - self.prev_yaw
-		self.prev_yaw = yaw
+		#print("new imu= ")
+		#print(self.w)
 		dt = (current_time - self.last_time).to_sec()
-		delta_x = (self.vx * cos(self.th) - self.vy * sin(self.th)) * dt
-		delta_y = (self.vx * sin(self.th) + self.vy * cos(self.th)) * dt
+		delta_x = (self.vx * math.cos(self.th) - self.vy * math.sin(self.th)) * dt
+		delta_y = (self.vx * math.sin(self.th) + self.vy * math.cos(self.th)) * dt
+		self.prev_yaw = yaw
 		delta_th = self.w * dt
 
 		self.x += delta_x
 		self.y += delta_y
-		self.th += delta_th
+		self.th += self.w
 		odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
 		odom = Odometry()
 		odom.header.stamp = current_time
@@ -118,7 +118,7 @@ class WASPMini(object):
 
 		# publish the message
 		self.wheel_odom_pub.publish(odom)
-		self.shape_pub.publish(cur_shape)
+		self.shape_pub.publish(self.cur_shape)
 		self.last_time = current_time
 
 
@@ -126,7 +126,7 @@ class WASPMini(object):
 def listener():
     rospy.init_node('smorphi_controller', anonymous=True)
     _object = WASPMini()
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(100)
     print("Test ok!")
     while not rospy.is_shutdown():
         _object.encoder_read()
